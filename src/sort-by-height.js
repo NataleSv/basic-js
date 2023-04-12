a66e85b31dbdeeb2b2c416f511711e966f76813b
Natalia Sorokina <nalya92@list.ru>
2023-04-12 14:55:17 +0300

    feat: sprt-by-height

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,7 +11,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
+function sortByHeight(arr) {
+
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] !== -1) {
+      newArr.push(arr[i]);
+    }
+  }
+
+  newArr.sort(function(a, b) {
+    return a - b;
+  });
+
+
+  let k = 0;
+  for(let j = 0; j < arr.length; j++) {
+    if (arr[j] !== - 1) {
+      arr[j] = newArr[k];
+      k++;
+    }
+  }
+  return arr;
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
