957201c3c33b878c9b7a201fc5d227a6bfbc724f
Natalia Sorokina <nalya92@list.ru>
2023-04-14 19:50:18 +0300

    feat: what-season

--- src/what-season.js
+++ src/what-season.js
@@ -11,8 +11,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+
+  if(!date) {
+    return 'Unable to determine the time of year!';
+  }
+
+  let newDate = new Date(date);
+  if(typeof date == 'string' ||
+    Object.prototype.toString.call(date) != '[object Date]' ||
+    Object.getOwnPropertySymbols(date).length !== 0) {
+       throw new Error ('Invalid date!');
+  }
+
+  const seasons = ['spring', 'summer', 'autumn (fall)', 'winter'];
+
+  let numbMonth = date.getMonth();
+
+  if(numbMonth == 11 || numbMonth <=1 ){
+    return seasons[3];
+
+  } else if (numbMonth > 1 && numbMonth < 5) {
+    return seasons[0];
+
+  } else if (numbMonth >=5 && numbMonth < 8) {
+    return seasons[1];
+
+  } else if (numbMonth >=8 && numbMonth < 11) {
+    return seasons[2];
+  } 
+
+
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
