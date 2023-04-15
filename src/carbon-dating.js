faa2c7f4d8d0f2368f389936645ef5d5ca9b1547
Natalia Sorokina <nalya92@list.ru>
2023-04-15 17:11:02 +0300

    feat: add carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,20 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+
+  let numb = Number(sampleActivity);
+  let age = Math.ceil( Math.log(MODERN_ACTIVITY / numb) * (HALF_LIFE_PERIOD / Math.LN2) );
+  
+  if (typeof sampleActivity != 'string' || 
+      isNaN(numb) ||
+      !(/^[\d.]+$/.test(sampleActivity)) ||
+      numb <=0 ||
+      numb >= MODERN_ACTIVITY ) {
+        return false;
+  } else {
+      return age; 
+  }
 }
 
 module.exports = {
