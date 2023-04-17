491b91fc5ed590dee1de420a9f379b55a0ce9c64
Natalia Sorokina <nalya92@list.ru>
2023-04-17 17:53:00 +0300

    feat: add extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+
+  //количество повторений строки str
+  let repeatTimes = options.repeatTimes || 1; 
+  //разделение повторений строки str               
+  let separator = options.separator || '+'; 
+  //доп. строка при повторении строки str
+  let addition = options.addition;  
+  //количество добавлений повторений  
+  let additionRepeatTimes = options.additionRepeatTimes || 1; 
+  // разделение addition           
+  let additionSeparator = options.additionSeparator || '|';  
+
+  let newStrArr = [];
+  let result;
+
+  for (let i = 0; i < repeatTimes; i++) {
+    let part = str;
+    if (addition !== undefined) {
+      for (let j = 0; j < additionRepeatTimes; j++) {
+        if (additionRepeatTimes > j+1) {
+          part += (addition + additionSeparator);
+        } else if (additionRepeatTimes == j+1) {
+          part += addition;
+        }
+      }
+    }
+    newStrArr.push(part);
+  }
+  result = newStrArr.join(separator);
+
+  return result;
 }
 
 module.exports = {
