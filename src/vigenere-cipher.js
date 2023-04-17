ba3e7f7cdc2c80695dffb54616e47a45b52fc43e
Natalia Sorokina <nalya92@list.ru>
2023-04-17 20:52:11 +0300

    feat: add vigenerr-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,101 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    this.type = type;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    
+    if (!message ||
+        typeof message !== 'string' ||
+        !key || 
+        typeof key !== 'string') {
+          throw new Error ("Incorrect arguments!");
+        }
+        
+    let m = message.toUpperCase().split(' ').join(''); 
+    let k = key.toUpperCase(); 
+    let res = ''; 
+    let result = '';
+
+    while(m.length > k.length) k+=k
+    
+    for (let i = 0; i < m.length; i++) {
+      let newAlp = [...this.alphabet].slice('');
+      let codeAlp = newAlp.concat( newAlp.splice(0, newAlp.indexOf(k[i])) ).join('');
+
+      if (this.alphabet.includes(m[i])) {
+        res += codeAlp[this.alphabet.indexOf(m[i])];
+      } else {
+        res += m[i];
+      }
+    }
+    
+    let revM = res.split('').reverse();
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i] != ' ') {
+        result += revM.pop();
+      } else {
+        result += ' ';
+      }
+    }
+
+    if (this.type) {
+      return result;
+    } else {
+      result = result.split('').reverse().join('');
+      return result;
+    }
+   
   }
-}
+  
+  decrypt(message, key) {
+    if (!message ||
+        typeof message !== 'string' ||
+        !key || 
+        typeof key !== 'string') {
+          throw new Error ("Incorrect arguments!");
+    }
+        
+    let m = message.toUpperCase().split(' ').join('');
+    let k = key.toUpperCase();  
+    let res = '';  
+    let result = '';
+
+    while(m.length > k.length) k+=k
+
+    for (let i = 0; i < m.length; i++) {
+      let newAlp = [...this.alphabet].slice('');
+      let codeAlp = newAlp.concat(newAlp.splice(0, newAlp.indexOf(k[i]))).join('');
+
+      if (this.alphabet.includes(m[i])) {
+        res += this.alphabet[codeAlp.indexOf(m[i])];
+      } else {
+        res += m[i];
+      }
+    }
+    
+    let revM = res.split('').reverse();
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i] != ' ') {
+        result += revM.pop();
+      } else {
+        result += ' ';
+      }
+    }
+    
+    if (this.type) {
+      return result;
+    } else {
+      result = result.split('').reverse().join('');
+      return result;
+    }
+  }
+  }
+
 
 module.exports = {
   VigenereCipheringMachine
